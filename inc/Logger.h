92964b3892642d3cc00961a47b3e87961b554c6d
Ricky <god1w@outlook.com>
2020-11-09 16:03:32 -0500

    update

--- inc/Logger.h
+++ inc/Logger.h
@@ -7,71 +7,71 @@
 #include <mutex>
 #include <string>
 
-using namespace std;
-
 namespace log {
+
+using namespace std;
 
 enum class LogLevel { FATAL = 0, ERROR, WARN, INFO, DEBUG };
 
 class nocopyable {
-   protected:
-    nocopyable() = default;
-    ~nocopyable() = default;
+ protected:
+  nocopyable() = default;
+  ~nocopyable() = default;
 
-   private:
-    nocopyable(const nocopyable&) = delete;
-    nocopyable& operator=(const nocopyable&) = delete;
+ private:
+  nocopyable(const nocopyable&) = delete;
+  nocopyable& operator=(const nocopyable&) = delete;
 };
 
 class Logger : nocopyable {
-   public:
-    Logger(string FILE, string FUNCTION, int LINE,
-           LogLevel LogLevel = LogLevel::INFO);
-    ~Logger();
+ public:
+  Logger(string FILE, string FUNCTION, int LINE,
+         LogLevel LogLevel = LogLevel::INFO);
+  ~Logger();
 
-    Logger& operator<<(string s);
+  Logger& operator<<(string s);
 
-    static void Init(bool isterminal = false);
-    static void Stop();
-    static bool HasLog();
-    static void SetColor(bool color);
+  static void Init(bool isterminal = false);
+  static void Stop();
+  static bool HasLog();
+  static void SetColor(bool color);
 
-   private:
-    LogLevel loglevel_;
+ private:
+  LogLevel loglevel_;
 
-    string log_line_;
+  string log_line_;
 
-    string FILE_;
-    string FUNCTION_;
-    int LINE_;
+  string FILE_;
+  string FUNCTION_;
+  int LINE_;
 
-    static bool write_terminal_;
+  static bool write_terminal_;
 
-    static string file_name_;
-    static fstream file_;
-    static string date_;
+  static string file_name_;
+  static fstream file_;
+  static string date_;
 
-    static string busy_buf_;
-    static string free_buf_;
+  static string busy_buf_;
+  static string free_buf_;
 
-    static condition_variable cond_;
-    static mutex mtx_;
-    static mutex mtx_f_;
-    static bool looping_;
-    static int interval_;
+  static condition_variable cond_;
+  static mutex mtx_;
+  static mutex mtx_f_;
+  static bool looping_;
+  static int interval_;
 
-    static bool color_;
+  static bool color_;
 
-    static void ThreadFunc();
+  static void ThreadFunc();
 
-    static void SetFileName(const string file_name = "log.log");
-    static void CheckDate();
-    static void FlushALL();
-    static string GetNowDate();
-    static string GetNowTime();
+  static void SetFileName(const string file_name = "log.log");
+  static void CheckDate();
+  static void FlushALL();
+  static string GetNowDate();
+  static string GetNowTime();
 
-    void Format();
-    void AddColor();
+  void Format();
+  void AddColor();
 };
 
 #define LOG() Logger(__FILE__, __FUNCTION__, __LINE__)
--- src/Logger.cc
+++ src/Logger.cc
@@ -9,9 +9,9 @@
 
 using namespace log;
 
-#define MAX_LOG_LINE_LEN 512
-#define MAX_LOG_BUF_SIZE 102400
-#define WAIT_INTERVAL 2
+constexpr int MAX_LOG_LINE_LEN = 512;
+constexpr int MAX_LOG_BUF_SIZE = 102400;
+constexpr int WAIT_INTERVAL = 2;
 
 const map<LogLevel, string> loglevel_str = {
     {LogLevel::FATAL, "FATAL"}, {LogLevel::ERROR, "ERROR"},
@@ -25,45 +25,45 @@ Logger::Logger(string FILE, string FUNCTION, int LINE, LogLevel loglevel)
       FILE_(FILE),
       FUNCTION_(FUNCTION),
       LINE_(LINE) {
-    log_line_.reserve(MAX_LOG_LINE_LEN);
-    CheckDate();
-    Format();
+  log_line_.reserve(MAX_LOG_LINE_LEN);
+  CheckDate();
+  Format();
 }
 
 Logger::~Logger() {
-    CheckDate();
-    if (Logger::busy_buf_.size() >= MAX_LOG_BUF_SIZE) {
-        // local scope
-        {
-            lock_guard<mutex> lk(Logger::mtx_);
-            swap(Logger::busy_buf_, Logger::free_buf_);
-        }
-        Logger::cond_.notify_one();
+  CheckDate();
+  if (Logger::busy_buf_.size() >= MAX_LOG_BUF_SIZE) {
+    // local scope
+    {
+      lock_guard<mutex> lk(Logger::mtx_);
+      swap(Logger::busy_buf_, Logger::free_buf_);
     }
-    log_line_ += "\n";
-    busy_buf_ += log_line_;
+    Logger::cond_.notify_one();
+  }
+  log_line_ += "\n";
+  busy_buf_ += log_line_;
 
-    if (Logger::write_terminal_) {
-        AddColor();
-        cerr << log_line_ << flush;
-    }
+  if (Logger::write_terminal_) {
+    AddColor();
+    cerr << log_line_ << flush;
+  }
 }
 
 Logger& Logger::operator<<(string s) {
-    log_line_ += " " + s;
-    return *this;
+  log_line_ += " " + s;
+  return *this;
 }
 
 void Logger::Init(bool isterminal) {
-    Logger::write_terminal_ = isterminal;
+  Logger::write_terminal_ = isterminal;
 
-    Logger::busy_buf_.reserve(MAX_LOG_BUF_SIZE);
-    Logger::free_buf_.reserve(MAX_LOG_BUF_SIZE);
+  Logger::busy_buf_.reserve(MAX_LOG_BUF_SIZE);
+  Logger::free_buf_.reserve(MAX_LOG_BUF_SIZE);
 
-    thread th(Logger::ThreadFunc);
-    if (th.joinable()) {
-        th.detach();
-    }
+  thread th(Logger::ThreadFunc);
+  if (th.joinable()) {
+    th.detach();
+  }
 }
 
 void Logger::Stop() { Logger::looping_ = false; }
@@ -90,114 +90,114 @@ int Logger::interval_ = WAIT_INTERVAL;
 bool Logger::color_ = false;
 
 void Logger::ThreadFunc() {
-    while (looping_) {
-        if (!file_.is_open()) {
-            file_ = fstream(Logger::file_name_, ios::app);
-        }
-        unique_lock<mutex> ulk(mtx_);
-        cond_.wait_for(ulk, Logger::interval_ * 1s,
-                       [] { return Logger::HasLog(); });
-
-        if (Logger::busy_buf_.size()) {
-            swap(Logger::busy_buf_, Logger::free_buf_);
-            // local scope
-            {
-                lock_guard<mutex> lk(Logger::mtx_f_);
-                Logger::file_ << Logger::free_buf_ << flush;
-            }
-            Logger::free_buf_.clear();
-            // cout << HasLog() << "busy " << Logger::free_buf_.str() << endl;
-        }
+  while (looping_) {
+    if (!file_.is_open()) {
+      file_ = fstream(Logger::file_name_, ios::app);
     }
+    unique_lock<mutex> ulk(mtx_);
+    cond_.wait_for(ulk, Logger::interval_ * 1s,
+                   [] { return Logger::HasLog(); });
 
-    Logger::FlushALL();
-
-    if (file_.is_open()) {
-        file_.close();
+    if (Logger::busy_buf_.size()) {
+      swap(Logger::busy_buf_, Logger::free_buf_);
+      // local scope
+      {
+        lock_guard<mutex> lk(Logger::mtx_f_);
+        Logger::file_ << Logger::free_buf_ << flush;
+      }
+      Logger::free_buf_.clear();
+      // cout << HasLog() << "busy " << Logger::free_buf_.str() << endl;
     }
+  }
+
+  Logger::FlushALL();
+
+  if (file_.is_open()) {
+    file_.close();
+  }
 }
 
 void Logger::CheckDate() {
+  if (Logger::date_ < Logger::GetNowDate()) {
+    lock_guard<mutex> lk(Logger::mtx_f_);
     if (Logger::date_ < Logger::GetNowDate()) {
-        lock_guard<mutex> lk(Logger::mtx_f_);
-        if (Logger::date_ < Logger::GetNowDate()) {
-            FlushALL();
-            Logger::date_ = Logger::GetNowDate();
-            Logger::file_name_ = Logger::date_ + ".log";
-            fstream new_file = fstream(Logger::file_name_, ios::app);
-            Logger::file_.swap(new_file);
-            if (new_file.is_open()) {
-                new_file.close();
-            }
-        }
+      FlushALL();
+      Logger::date_ = Logger::GetNowDate();
+      Logger::file_name_ = Logger::date_ + ".log";
+      fstream new_file = fstream(Logger::file_name_, ios::app);
+      Logger::file_.swap(new_file);
+      if (new_file.is_open()) {
+        new_file.close();
+      }
     }
+  }
 }
 
 void Logger::FlushALL() {
-    if (Logger::free_buf_.size() || busy_buf_.size()) {
-        if (!Logger::file_.is_open()) {
-            file_ = fstream(Logger::file_name_, ios::app);
-        }
+  if (Logger::free_buf_.size() || busy_buf_.size()) {
+    if (!Logger::file_.is_open()) {
+      file_ = fstream(Logger::file_name_, ios::app);
     }
-    if (Logger::free_buf_.size()) {
-        {
-            lock_guard<mutex> lk(Logger::mtx_f_);
-            Logger::file_ << Logger::free_buf_ << flush;
-        }
+  }
+  if (Logger::free_buf_.size()) {
+    {
+      lock_guard<mutex> lk(Logger::mtx_f_);
+      Logger::file_ << Logger::free_buf_ << flush;
     }
-    if (busy_buf_.size()) {
-        {
-            lock_guard<mutex> lk(Logger::mtx_f_);
-            Logger::file_ << Logger::free_buf_ << flush;
-        }
+  }
+  if (busy_buf_.size()) {
+    {
+      lock_guard<mutex> lk(Logger::mtx_f_);
+      Logger::file_ << Logger::free_buf_ << flush;
     }
+  }
 }
 
 string Logger::GetNowDate() {
-    char tmp[16];
-    time_t rawtime;
-    tm timeinfo;
-    time(&rawtime);
-    localtime_r(&rawtime, &timeinfo);
-    strftime(tmp, 16, "%Y%m%d", &timeinfo);
-    return move(string(string(tmp)));
+  char tmp[16];
+  time_t rawtime;
+  tm timeinfo;
+  time(&rawtime);
+  localtime_r(&rawtime, &timeinfo);
+  strftime(tmp, 16, "%Y%m%d", &timeinfo);
+  return move(string(string(tmp)));
 }
 
 string Logger::GetNowTime() {
-    char tmp[16];
-    time_t rawtime;
-    tm timeinfo;
-    time(&rawtime);
-    localtime_r(&rawtime, &timeinfo);
-    strftime(tmp, 16, "%X", &timeinfo);
-    return move(string(string(tmp)));
+  char tmp[16];
+  time_t rawtime;
+  tm timeinfo;
+  time(&rawtime);
+  localtime_r(&rawtime, &timeinfo);
+  strftime(tmp, 16, "%X", &timeinfo);
+  return move(string(string(tmp)));
 }
 
 void Logger::Format() {
-    log_line_ += Logger::date_ + " " + Logger::GetNowTime() + " " +
-                 loglevel_str.at(loglevel_) + " " + FILE_ + " " + FUNCTION_ +
-                 " " + to_string(LINE_);
+  log_line_ += Logger::date_ + " " + Logger::GetNowTime() + " " +
+               loglevel_str.at(loglevel_) + " " + FILE_ + " " + FUNCTION_ +
+               " " + to_string(LINE_);
 }
 
 void Logger::AddColor() {
-    if (Logger::color_) {
-        switch (Logger::loglevel_) {
-            case LogLevel::FATAL:
-                log_line_ = "\033[31m" + log_line_ + "\033[0m";
-                break;
-            case LogLevel::ERROR:
-                log_line_ = "\033[31m" + log_line_ + "\033[0m";
-                break;
-            case LogLevel::WARN:
-                log_line_ = "\033[33m" + log_line_ + "\033[0m";
-                break;
-            case LogLevel::INFO:
-                break;
-            case LogLevel::DEBUG:
-                log_line_ = "\033[32m" + log_line_ + "\033[0m";
-                break;
-            default:
-                break;
-        }
+  if (Logger::color_) {
+    switch (Logger::loglevel_) {
+      case LogLevel::FATAL:
+        log_line_ = "\033[31m" + log_line_ + "\033[0m";
+        break;
+      case LogLevel::ERROR:
+        log_line_ = "\033[31m" + log_line_ + "\033[0m";
+        break;
+      case LogLevel::WARN:
+        log_line_ = "\033[33m" + log_line_ + "\033[0m";
+        break;
+      case LogLevel::INFO:
+        break;
+      case LogLevel::DEBUG:
+        log_line_ = "\033[32m" + log_line_ + "\033[0m";
+        break;
+      default:
+        break;
     }
+  }
 }
